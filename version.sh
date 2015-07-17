3ab231822d04f057f7985dc07f52208b2e131ffc
Gabe Ferencz <gabe.public@gmail.com>
2015-07-17 15:35:59 -0400

    version: Make color output optional with -c flag

--- version.sh
+++ version.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 # version.sh - Print out various system version information
 
+while getopts ":c" opt; do
+	case "$opt" in
+		c)
+			SET_COLOR=$(tput setf 2)
+			CLR_COLOR=$(tput sgr 0)
+			;;
+	esac
+done
+	
 echo_version() {
-	echo "$(tput setf 2)$1$(tput sgr 0)$2"
+	echo "$SET_COLOR$1$CLR_COLOR$2"
 }
 
 ldd_vers="$(ldd --version | head -n 1)"
